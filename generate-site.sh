bc5112ebd9ae13c400ebf4b5cd61f50bcdfa7c6b
Dave Moten <davidmoten@gmail.com>
2022-10-23 15:07:22 +1100

    fix generate-site.sh

--- generate-site.sh
+++ generate-site.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
-cd rxjava2-jdbc
+cd rxjava3-jdbc
 NAME=${PWD##*/}
 mvn site
 cd ../../davidmoten.github.io
 git pull
 mkdir -p $NAME
-cp -r ../$NAME/rxjava2-jdbc/target/site/* $NAME/
+cp -r ../$NAME/rxjava3-jdbc/target/site/* $NAME/
 git add .
 git commit -am "update site reports"
 git push
